414fe5aa5eccd7b81fc05b999705758a9011073b
PMPDEV <fatforbespmp@gmail.com>
2026-09-09 06:52:23 +0100

    @

---
+++ PoliceMP-Resources/[EUP]/pmp_fullclothing/fxmanifest.lua
@@ -0,0 +1,14 @@
+-- Generated with DurtyClothTool
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_m_freemode_01_mp_m_pmp_fullclothing.meta',
+  'mp_f_freemode_01_mp_f_pmp_fullclothing.meta',
+  'stream/pedalternatevariations.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_pmp_fullclothing.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_pmp_fullclothing.meta'
+data_file 'ALTERNATE_VARIATIONS_FILE' 'stream/pedalternatevariations.meta'
