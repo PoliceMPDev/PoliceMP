12a413978c464562580653776ad624ed4dd3b8ac
PMPDEV <fatforbespmp@gmail.com>
2026-09-09 08:57:58 +0100

    @

---
+++ PoliceMP-Resources/[roxwood]/amb-roxwood-props/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+this_is_a_map 'yes'
+
+lua54 'yes'
+
+file 'stream/amb_rox_props/amb_rox_props.ytyp'
+file 'stream/amb_rox_int_props/amb_rox_int_props.ytyp'
+file 'stream/amb_rox_props/amb_rox_props_lights.ytyp'
+file 'stream/amb_rox_props_roadsigns/amb_rox_props_roadsigns.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/amb_rox_props/amb_rox_props.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/amb_rox_int_props/amb_rox_int_props.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/amb_rox_props/amb_rox_props_lights.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/amb_rox_props_roadsigns/amb_rox_props_roadsigns.ytyp'
+dependency '/assetpacks'
