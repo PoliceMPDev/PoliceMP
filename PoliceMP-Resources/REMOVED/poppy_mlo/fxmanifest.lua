aa9fb78a9307e34d5e667ac57cdfa0a78992abd7
PMPDEV <fatforbespmp@gmail.com>
2026-09-09 09:59:36 +0100

    @

---
+++ PoliceMP-Resources/REMOVED/poppy_mlo/fxmanifest.lua
@@ -0,0 +1,8 @@
+version '1.0.0'
+fx_version 'adamant'
+games { 'gta5' }
+
+this_is_a_map 'yes'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/poppy/custom_poppy.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/poppy/custom_poppy_wreath.ytyp'
