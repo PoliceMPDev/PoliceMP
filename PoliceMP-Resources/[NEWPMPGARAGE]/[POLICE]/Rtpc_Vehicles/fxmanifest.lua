961e2d8d039add7a83ff512a65badcd93a815ecd
PMPDEV <fatforbespmp@gmail.com>
2026-09-09 08:40:43 +0100

    @

---
+++ PoliceMP-Resources/[NEWPMPGARAGE]/[POLICE]/Rtpc_Vehicles/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+    '**/**.meta',
+    '**/**.meta',
+ 
+}
+
+
+--data_file 'HANDLING_FILE' '**/**.meta'
+data_file 'VEHICLE_METADATA_FILE' '**/**.meta'
+data_file 'VEHICLE_VARIATION_FILE' '**/**.meta'
+
+
+dependency '/assetpacks'
+dependency '/assetpacks'
+dependency '/assetpacks'
