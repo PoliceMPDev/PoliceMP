4e50c9d8df1c2db5ebfb86916bd687ceb7919882
PMPDEV <fatforbespmp@gmail.com>
2026-09-09 08:18:08 +0100

    @

---
+++ PoliceMP-Resources/[NEWPMPGARAGE]/[DEV]/Dev_Garage/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+files {
+    'vehicles.meta',
+	'carvariations.meta',
+	'handling.meta',
+	'carcols.meta',
+ 
+}
+
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+
+dependency '/assetpacks'
+dependency '/assetpacks'
+dependency '/assetpacks'
